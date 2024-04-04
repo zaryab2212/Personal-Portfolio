e9ef8242a5ad69b1bdbd9362b5500eecd6bdb282
zaryab ikram <zaryabsheikh0@gmail.com>
2024-04-04 16:52:35 +0500

    mogo falut

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 exports.db = async () => {
   try {
-    await mongoose.connect(process.env.MOGO_URI);
+    // await mongoose.connect(process.env.MOGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://zaryab2212:Hangover123@portfolio.ctz5rvs.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Db is connected");
   } catch (error) {
     console.log(error);
